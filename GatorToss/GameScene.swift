f147b2053f4288a739f301b820369ad400f97c83
Eric Smith <ericsmith1302@gmail.com>
2015-02-18 12:58:47 -0800

    Made UIColor for hex

--- GatorToss/GameScene.swift
+++ GatorToss/GameScene.swift
@@ -59,3 +59,19 @@ class GameScene: SKScene {
         /* Called before each frame is rendered */
     }
 }
+
+extension UIColor {
+
+    convenience init(hex: Int) {
+
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+
+    }
+
+}
