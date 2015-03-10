345adddadb8dd3a8cbaadbe688e0e3a78ad31157
Eric Smith <ericsmith1302@gmail.com>
2015-03-10 13:18:34 -0700

    Added camera, and throwing action

--- GatorToss/GameScene.swift
+++ GatorToss/GameScene.swift
@@ -12,15 +12,19 @@ let friction:CGFloat = 0.25
 
 class GameScene: SKScene {
 
+    var world:SKNode!
     var tebow:Tebow!
     var river1:SKSpriteNode!
-    var river2:SKSpriteNode!
+    var mascot:SKSpriteNode!
     var ground:SKSpriteNode!
     var sprite:SKSpriteNode!
+    var rotator:SKSpriteNode!
+    let mascotSize:CGFloat = 30
     var groundWidth:CGFloat = 600
     var offsetX:CGFloat! = 0
     var offsetY:CGFloat = 0
     var runButton:UIButton!
+    var worldPos:CGPoint!
 
     override func didMoveToView(view: SKView) {
         
@@ -29,22 +33,18 @@ class GameScene: SKScene {
         self.backgroundColor = UIColor(red: 135/255.0, green: 187/255.0, blue: 222/255.0, alpha: 1)
         self.physicsWorld.gravity = CGVectorMake(0, -3)
         
-        //Make a river1
-
-        river1 = SKSpriteNode(color:UIColor.blueColor(), size: CGSizeMake(self.frame.size.width+7, self.frame.size.height/2.5-25))
-        river1.anchorPoint = CGPointMake(0,0)
-        river1.position = CGPointMake(0, 0)
+        // World Scene (basically our camera)
+        world = SKNode()
+        world.position = CGPointMake(0, 0)
+        self.addChild(world)
+        worldPos = world.position
+        
+        // Make a river1
+        river1 = SKSpriteNode(color:UIColor.blueColor(), size: CGSizeMake(self.frame.size.width*2, self.frame.size.height/2.5-25))
+        river1.position = CGPointMake(river1.size.width/2, river1.size.height/2)
         river1.xScale = 1
         river1.yScale = 1
-        self.addChild(river1)
-        
-        //Make a river2
-        river2 = SKSpriteNode(color:UIColor(hex: 0x2EB6DB), size: CGSizeMake(self.frame.size.width+7, self.frame.size.height/2.5-25))
-        river2.anchorPoint = CGPointMake(0,0)
-        river2.position = CGPointMake(frame.size.width-1, 0)
-        river2.xScale = 1
-        river2.yScale = 1
-        self.addChild(river2)
+        world.addChild(river1)
 
         // Make ground
         ground = SKSpriteNode(color: UIColor.greenColor(), size: CGSizeMake(groundWidth, self.frame.size.height/2.5))
@@ -54,23 +54,24 @@ class GameScene: SKScene {
         ground.yScale = 1
         ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.size)
         ground.physicsBody!.dynamic = false
-        self.addChild(ground)
+        world.addChild(ground)
         
         //let tebowSprite = SKSpriteNode(imageNamed:"Tebow")
         //tebowSprite.anchorPoint = CGPointMake(0, 0)
         //tebowSprite.xScale = -0.2
         //tebowSprite.yScale = 0.2
         let tebowSprite = SKSpriteNode(color: UIColor.redColor(), size: CGSizeMake(35, 70))
+        // Cant use anchor point on physics body. This means the default anchor point is in the middle of tebow instead of the corners
         tebowSprite.position = CGPointMake(50 + tebowSprite.size.width/2, ground.frame.origin.y + ground.size.height + tebowSprite.size.height/2 + 10)
         tebowSprite.physicsBody = SKPhysicsBody(circleOfRadius: tebowSprite.frame.size.height/2)
         tebowSprite.physicsBody!.dynamic = true
         tebowSprite.physicsBody?.allowsRotation = false
         tebowSprite.physicsBody?.friction = 0.5
-        self.addChild(tebowSprite)
+        world.addChild(tebowSprite)
         tebow = Tebow(x: tebowSprite.frame.size.width + 50, y: self.frame.size.height/2.5 - 20, sprite: tebowSprite)
         
         // Make Rotator
-        let rotator = SKSpriteNode(color: UIColor.yellowColor(), size: CGSizeMake(40, 5))
+        rotator = SKSpriteNode(color: UIColor.yellowColor(), size: CGSizeMake(40, 5))
         rotator.anchorPoint = CGPointMake(0, 0.5)
         tebowSprite.addChild(rotator)
         // Rotate animation
@@ -79,8 +80,15 @@ class GameScene: SKScene {
         let degToRad:CGFloat = 0.0175
         let rotateUp = SKAction.rotateToAngle(80*degToRad, duration: duration)
         let rotateDown = SKAction.rotateToAngle(5*degToRad, duration: duration)
-        let sequence = SKAction.repeatActionForever(SKAction.sequence([rotateUp, rotateDown]))
-        rotator.runAction(sequence)
+        let rotateSequence = SKAction.repeatActionForever(SKAction.sequence([rotateUp, rotateDown]))
+        rotator.runAction(rotateSequence, withKey: "rotateSequence")
+        
+        // Make Mascot
+        mascot = SKSpriteNode(color:UIColor.orangeColor(), size: CGSizeMake(mascotSize, mascotSize))
+        mascot.position = CGPointMake(mascotSize/2,mascotSize/2)
+        mascot.xScale = 1
+        mascot.yScale = 1
+        world.addChild(mascot)
         
         //Make running button
         let runButtonSize:CGFloat = 50
@@ -92,7 +100,7 @@ class GameScene: SKScene {
         
         //Make throwing button
         let throwButtonSize:CGFloat = 50
-        let throwButton = UIButton(frame: CGRectMake(600, self.view!.frame.size.height - throwButtonSize - 25, throwButtonSize, throwButtonSize))
+        let throwButton = UIButton(frame: CGRectMake(self.view!.frame.size.width-throwButtonSize-25, self.view!.frame.size.height - throwButtonSize - 25, throwButtonSize, throwButtonSize))
         throwButton.layer.cornerRadius = throwButtonSize/2
         throwButton.backgroundColor = UIColor(hex: 0xFFBE63)
         throwButton.addTarget(self, action: "throwButtonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
@@ -101,21 +109,35 @@ class GameScene: SKScene {
     
     //when runButton is clicked
     func runButtonClicked() {
-        println("Pressed")
         tebow.sprite.physicsBody?.applyImpulse(CGVectorMake(6, 0))
+        rotator.removeActionForKey("rotateSequence")
     }
     
     //when throwButton is clicked
     //need to release the mascot
     func throwButtonClicked(Button:UIButton){
-        println("Throw clicked")
+        rotator.removeActionForKey("rotateSequence")
         runButton.removeFromSuperview()
         Button.removeFromSuperview()
+        tebow.didThrow = true
+        // Throw mascot
+        mascot.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(mascotSize, mascotSize))
+        mascot.physicsBody!.dynamic = true
+        mascot.physicsBody?.allowsRotation = true
+        //mascot.physicsBody?.friction = 0.5
+        mascot.physicsBody?.applyImpulse(CGVectorMake(tebow.getXPower(rotator.zRotation), tebow.getYPower(rotator.zRotation)))
+        println(tebow.getVelocityBonus())
+        // Stop tebow from sliding
+        tebow.sprite.physicsBody = nil
+        self.centerOnNode(tebow.sprite)
+        // Make the water a physics block
+        river1.physicsBody = SKPhysicsBody(rectangleOfSize: river1.size)
+        river1.physicsBody!.dynamic = false
+
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
         }
@@ -125,40 +147,56 @@ class GameScene: SKScene {
         /* Called before each frame is rendered */
         
         //moves the ground 1 position at a time to left
-//        if(ground.position.x < -groundWidth) {
-//            self.removeChildrenInArray([ground])
-//        }
-//        ground.position.x = ground.position.x - 10
-//        
-//        //moves Tim Tebow 1 position at a time to left
-//        //COME BACK TO THIS
+        if(ground.position.x < -groundWidth) {
+            self.removeChildrenInArray([ground])
+        }
+        //ground.position.x = ground.position.x - 10
+        
+        //moves Tim Tebow 1 position at a time to left
+        //COME BACK TO THIS
 //        if(tebow.sprite.position.x < -tebow.sprite.frame.size.width){
 //            self.removeChildrenInArray([tebow.sprite])
 //        }
 //        tebow.sprite.position.x = tebow.sprite.position.x - 10
-//        
-//       
-//        //moves river1 1 position at a time to left
-//        river1.position.x = river1.position.x - 10
-//        if(river1.position.x <= 1-self.frame.size.width){
-//            river1.position.x = self.frame.size.width+1
-//        }
-//        
-//        //moves river2 1 position at a time to left
-//        river2.position.x = river2.position.x - 10
-//        if(river2.position.x <= 1-self.frame.size.width){
-//            river2.position.x = self.frame.size.width+1
-//        }
-
-        if tebow.xVel > 0 {
-            tebow.xVel -= friction
-        } else {
-            tebow.xVel = 0
-        }
-        tebow.x += tebow.xVel
-        //tebow.sprite.position.x = tebow.x
         
+       
+        //moves river1 1 position at a time to left
+        //river1.position.x = river1.position.x - 10
+        let river1Pos = self.convertPoint(river1.position, fromNode: world)
+        if(river1Pos.x <= 0){
+            river1.position.x += river1.frame.size.width/2
+        }
+        
+        if !tebow.didThrow {
+            mascot.position.x = tebow.sprite.position.x + tebow.sprite.frame.width/2
+            mascot.position.y = tebow.sprite.position.y + tebow.sprite.frame.height/2
+            self.centerOnNode(tebow.sprite)
+        } else {
+            self.centerOnNode(mascot)
+        }
+        
+        // Animate camera to position. Animating to prevent jerkiness
+        var diffX = worldPos.x - world.position.x
+        var diffY = worldPos.y - world.position.y
+        world.position.x += diffX/6
+        world.position.y += diffY/6
     }
+    
+    func sceneRelativePosition(node:SKSpriteNode) -> CGPoint {
+        var nodePositionInScene = node.scene!.convertPoint(node.position, fromNode: node.scene!)
+        return nodePositionInScene
+    }
+    
+    func centerOnNode(node:SKSpriteNode) {
+        var nodePositionInScene = sceneRelativePosition(node)
+        let centerOffsetX = nodePositionInScene.x - node.scene!.size.width/2
+        let centerOffsetY = nodePositionInScene.y - node.scene!.size.height/2
+        if centerOffsetX < 0 {
+            return
+        }
+        worldPos = CGPointMake(-1*centerOffsetX, -1*centerOffsetY);
+    }
+    
 }
 
 extension UIColor {
--- GatorToss/Tebow.swift
+++ GatorToss/Tebow.swift
@@ -16,12 +16,49 @@ class Tebow {
     var y:CGFloat
     var xVel:CGFloat
     var sprite:SKSpriteNode
+    var didThrow:Bool
+    var power:CGFloat
     
     init(x:CGFloat, y:CGFloat, sprite:SKSpriteNode) {
         self.x = x
         self.y = y
         xVel = 0
         self.sprite = sprite
+        self.didThrow = false
+        self.power = 10
+    }
+    
+    func radToDegree(rad:CGFloat) -> CGFloat {
+        let conversion:CGFloat = CGFloat(180/M_PI)
+        let degree:CGFloat = rad*conversion
+        return degree
+    }
+    
+    func getXPower(rad:CGFloat) -> CGFloat {
+        let xRatio = cos(rad)
+        return self.getVelocityBonus()*power*xRatio
+    }
+    
+    func getYPower(rad:CGFloat) -> CGFloat {
+        let yRatio = sin(rad)
+        return self.getVelocityBonus()*power*yRatio
+    }
+    
+    func getXVelocity() -> CGFloat? {
+        return self.sprite.physicsBody?.velocity.dx
+    }
+    
+    func getVelocityBonus() -> CGFloat {
+        let cap:CGFloat = 200
+        let capBonus:CGFloat = 7
+        
+        if let xVel = self.getXVelocity() {
+            let ratio = xVel/cap
+            var speedBonus = capBonus * ratio
+            return (speedBonus < 1) ? 1 : speedBonus
+        } else {
+            return 1
+        }
     }
     
 }
