ba954fe961755af5f3e43ac7d733cb4759c505e9
NG38 <NG38@NG38s-Mac.local>
2015-03-17 15:02:34 -0400

    CoachsCornerViewController

--- GatorToss/CoachsCornerViewController.swift
+++ GatorToss/CoachsCornerViewController.swift
@@ -1,16 +1,14 @@
 //
-//  UpgradeViewController.swift
+//  CoachsCornerViewController.swift
 //  GatorToss
 //
 //  Created by NG38 on 3/17/15.
 //  Copyright (c) 2015 SeniorDesign. All rights reserved.
 //
 
-//COACH'S CORNER
-
 import UIKit
 
-class UpgradeViewController: UIViewController {
+class CoachsCornerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
